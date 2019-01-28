abb82aacc94c0d15b2b21119063870e5a77017eb
Tom Holmes <tholmes59@gmail.com>
2019-01-28 16:59:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,14 +7,14 @@ class Artist
     @songs = []
   end 
   
-  def add_song_by_name(song)
-    @songs << song 
-    song.artist = self
-  end
-  
   def add_song(song)
     @songs << song 
     song.artist = self
   end 
+  
+  def add_song_by_name(song)
+    @songs << song 
+    song.artist = self
+  end
   
 end
