48c343e99035ce4903367054f6772427a1ff0f43
Tom Holmes <tholmes59@gmail.com>
2019-01-28 18:25:18 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,21 @@
 class Post 
   
+  attr_accessor :artist, :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def artist_name 
+    
+  end
   
 end
